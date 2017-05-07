abb1b71c74120e9b3e7ee962c5d1186b9798fac0
Jieyi <pokkbaby@gmail.com>
2017-05-07 17:25:44 +0900

    Added the dependency we need.

---
+++ ssfm/app/src/main/java/taiwan/no1/app/MainActivity.kt
@@ -0,0 +1,13 @@
+package taiwan.no1.app
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class MainActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
