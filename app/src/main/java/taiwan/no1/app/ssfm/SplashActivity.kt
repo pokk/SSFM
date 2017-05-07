3b0fbc8400c5be102d96bd5a793ae921862d33ba
Jieyi <pokkbaby@gmail.com>
2017-05-07 21:02:16 +0900

    Added the splash view.

---
+++ app/src/main/java/taiwan/no1/app/ssfm/SplashActivity.kt
@@ -0,0 +1,31 @@
+package taiwan.no1.app.ssfm
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+
+/**
+ *
+ * @author  jieyi
+ * @since   5/7/17
+ */
+
+class SplashActivity: Activity() {
+    companion object {
+        private val SPLASH_DELAY_TIME: Long = 500
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // Delay 0.5 second for showing the splash view.
+        Handler().postDelayed({
+            this.let {
+                // Transfer to your main activity after delay 0.5 second.
+                it.startActivity(Intent(this, MainActivity::class.java))
+                it.finish()
+            }
+        }, SPLASH_DELAY_TIME)
+    }
+}
