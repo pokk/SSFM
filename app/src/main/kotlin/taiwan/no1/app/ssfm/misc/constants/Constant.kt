891e6e0c00bfeb48eced205392897abdc1d5677b
Jieyi <pokkbaby@gmail.com>
2017-05-09 18:15:54 +0900

    Added new empty files for keeping folder path.

---
+++ app/src/main/kotlin/taiwan/no1/app/ssfm/misc/constants/Constant.kt
@@ -0,0 +1,10 @@
+package taiwan.no1.app.ssfm.misc.constants
+
+/**
+ * Customized constant variables.
+ *
+ * @author  jieyi
+ * @since   5/9/17
+ */
+
+object Constant
---
+++ app/src/main/kotlin/taiwan/no1/app/ssfm/misc/utilies/ImageLoader/ImageLoader.kt
@@ -0,0 +1,10 @@
+package taiwan.no1.app.ssfm.misc.utilies.ImageLoader
+
+/**
+ * Third-party image loader utils.
+ *
+ * @author  jieyi
+ * @since   5/9/17
+ */
+
+class ImageLoader
