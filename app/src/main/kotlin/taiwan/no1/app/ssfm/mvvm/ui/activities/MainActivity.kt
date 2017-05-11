4dfa3e67af6eb96d877c8dc007605587d1740730
Jieyi <pokkbaby@gmail.com>
2017-05-11 18:24:20 +0900

    Removed the rxjava1 dependency.

--- app/src/main/kotlin/taiwan/no1/app/ssfm/mvvm/ui/activities/MainActivity.kt
+++ app/src/main/kotlin/taiwan/no1/app/ssfm/mvvm/ui/activities/MainActivity.kt
@@ -2,6 +2,13 @@ package taiwan.no1.app.ssfm.mvvm.ui.activities
 
 import android.databinding.DataBindingUtil
 import android.os.Bundle
+import com.devrapid.kotlinknifer.AppLog
+import de.umass.lastfm.Artist
+import de.umass.lastfm.Authenticator
+import de.umass.lastfm.Caller
+import io.reactivex.Observable
+import io.reactivex.ObservableOnSubscribe
+import io.reactivex.schedulers.Schedulers
 import taiwan.no1.app.ssfm.R
 import taiwan.no1.app.ssfm.databinding.ActivityMainBinding
 import taiwan.no1.app.ssfm.mvvm.ui.BaseActivity
@@ -16,5 +23,30 @@ class MainActivity: BaseActivity() {
                 R.layout.activity_main)
 //        binding.setVariable(BR.viewmodel, MainViewModel(this.applicationContext))
         binding.viewmodel = MainViewModel(this.applicationContext)
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        // NOTE: 5/11/17 We can use the cache as like this way.
+//        Caller.getInstance().cache = FileSystemCache(File("${Environment.getExternalStorageDirectory()}/.lastfm"))
+        Caller.getInstance().cache = null
+        Caller.getInstance().userAgent = "tst"
+
+        val user = this.getString(R.string.lastfm_name)
+        val password = this.getString(R.string.lastfm_password)
+        val key = this.getString(R.string.lastfm_key)
+        val secret = this.getString(R.string.lastfm_secret_key)
+
+        Observable.just("").subscribeOn(Schedulers.computation()).map {
+            val session = Authenticator.getMobileSession(user, password, key, secret)
+            AppLog.w(session)
+            val top = Artist.getTopAlbums("ladygaga", session.apiKey)
+            AppLog.d(top)
+        }.subscribe {
+            AppLog.i(Thread.currentThread())
+        }
+
+        val o = Observable.create(ObservableOnSubscribe<String> { it.onNext("string") }).subscribe { AppLog.w(it) }
     }
 }
