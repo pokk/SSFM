774589242437a0203c76071a37f807ce712300c1
Jieyi <pokkbaby@gmail.com>
2017-05-15 19:09:43 +0900

    Modified the share preference delegate.
    1. Added the retrieve session.

--- app/src/main/kotlin/taiwan/no1/app/ssfm/mvvm/models/data/IDateStore.kt
+++ app/src/main/kotlin/taiwan/no1/app/ssfm/mvvm/models/data/IDateStore.kt
@@ -10,5 +10,15 @@ import io.reactivex.Observable
  * @since   5/10/17
  */
 interface IDateStore {
+    /**
+     * Retrieve the user session(This session's expired date is no limitation).
+     *
+     * @param user user name.
+     * @param pwd user password.
+     * @param key api key.
+     * @param secret
+     * @return The user session.
+     */
+    // NOTE: 5/13/17 We should keep the 'Session' and 'UserName' in the shared preferences.
     fun obtainSession(user: String, pwd: String, key: String, secret: String): Observable<Session>
 }
