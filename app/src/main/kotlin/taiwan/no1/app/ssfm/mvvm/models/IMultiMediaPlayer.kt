46c0704835273d71459750072ada6c92ef87c9eb
Weian <d92434101@gmail.com>
2017-06-18 18:21:26 +0800

    add abstract functions for interfaces.

--- app/src/main/kotlin/taiwan/no1/app/ssfm/mvvm/models/IMultiMediaPlayer.kt
+++ app/src/main/kotlin/taiwan/no1/app/ssfm/mvvm/models/IMultiMediaPlayer.kt
@@ -1,7 +1,18 @@
 package taiwan.no1.app.ssfm.mvvm.models
 
 /**
+ * For controlling multi-media player
+ *
  * Created by weian on 2017/6/18.
  */
 interface IMultiMediaPlayer {
+    fun play(uri: String)
+    fun stop()
+    fun pause()
+    fun resume()
+    fun replay()
+    fun seekto()
+    fun duration()
+    fun setLoop(is_loop: Boolean)
+    fun isLooping(): Boolean
 }
--- app/src/main/kotlin/taiwan/no1/app/ssfm/mvvm/models/IPlayerHander.kt
+++ app/src/main/kotlin/taiwan/no1/app/ssfm/mvvm/models/IPlayerHander.kt
@@ -1,7 +1,29 @@
 package taiwan.no1.app.ssfm.mvvm.models
 
 /**
+ * For controlling multi-media player and playlist.
+ *
  * Created by weian on 2017/6/18.
  */
 interface IPlayerHander {
+    fun play()
+    fun stop()
+    fun pause()
+    fun resume()
+    fun previous()
+    fun next()
+    fun replay()
+    fun seekTo()
+    fun duration()
+    fun downloadProcess()
+    fun totalMusicTime()
+    fun loopOne()
+    fun loopAll()
+    fun random()
+    fun playerStatus()
+    fun nowPlaying()
+    fun isLooping()
+    fun isRandom()
+    fun restTime()
+    fun setPlayList()
 }
