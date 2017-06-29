127bf0cad42dc0c7c414620c20a67000c49e6db4
Jieyi <pokkbaby@gmail.com>
2017-06-29 09:58:18 +0900

    Added the components in the player controller.

--- app/src/main/kotlin/taiwan/no1/app/ssfm/customized/PlayerControllerLayout.kt
+++ app/src/main/kotlin/taiwan/no1/app/ssfm/customized/PlayerControllerLayout.kt
@@ -1,10 +1,19 @@
 package taiwan.no1.app.ssfm.customized
 
 import android.content.Context
+import android.graphics.Color
 import android.util.AttributeSet
+import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
+import android.widget.ImageView
+import com.devrapid.kotlinknifer.logd
 import com.devrapid.kotlinknifer.logw
+import org.jetbrains.anko.backgroundColor
+import org.jetbrains.anko.collections.forEachWithIndex
 import org.jetbrains.anko.imageButton
+import org.jetbrains.anko.imageResource
+import org.jetbrains.anko.sdk25.coroutines.onClick
 import taiwan.no1.app.ssfm.R
 
 /**
@@ -13,11 +22,17 @@ import taiwan.no1.app.ssfm.R
  * @since   6/28/17
  */
 class PlayerControllerLayout: ViewGroup {
-    val listImageButtons = listOf(imageButton(R.drawable.ic_repeat),
+    val propertis: ImageButton.() -> Unit = {
+        this.backgroundColor = Color.TRANSPARENT
+        this.scaleType = ImageView.ScaleType.CENTER
+    }
+    val listImageButtons = listOf(imageButton(R.drawable.ic_repeat, propertis),
         imageButton(R.drawable.ic_skip_previous),
         imageButton(R.drawable.ic_play_circle),
         imageButton(R.drawable.ic_skip_next),
         imageButton(R.drawable.ic_shuffle))
+    var isPlayingState = false
+    var isRepeatingAll = true
 
     init {
     }
@@ -35,7 +50,6 @@ class PlayerControllerLayout: ViewGroup {
     }
 
     fun init(context: Context, attrs: AttributeSet?, defStyleAttr: Int) {
-//        this.listImageButtons.forEach(this::addView)
     }
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
@@ -43,13 +57,41 @@ class PlayerControllerLayout: ViewGroup {
 
         logw(MeasureSpec.toString(widthMeasureSpec), MeasureSpec.toString(heightMeasureSpec))
 
-        (0..this.childCount - 1).forEach {
-            this.measureChild(this.getChildAt(it), widthMeasureSpec, heightMeasureSpec)
-        }
-
+        this.measureChildren(widthMeasureSpec, heightMeasureSpec)
     }
 
     override fun onLayout(changed: Boolean, l: Int, t: Int, r: Int, b: Int) {
-        val childrenCount = this.childCount
+        if (5 < this.childCount)
+            return
+
+        logd(l, t, r, b)
+
+        this.listImageButtons.forEachWithIndex { index, _ ->
+            val childView = this.getChildAt(index)
+            layoutParams = childView.layoutParams
+            logd(childView, layoutParams)
+            if (View.GONE == childView.visibility)
+                return@forEachWithIndex
+
+            logw(childView.measuredWidth, childView.measuredHeight)
+        }
     }
+
+    fun setRepeatOnClick(listener: (ImageButton) -> Unit) = this.listImageButtons[0].onClick {
+        listener(it as ImageButton)
+        this@PlayerControllerLayout.isRepeatingAll = !this@PlayerControllerLayout.isRepeatingAll
+        it.imageResource = if (this@PlayerControllerLayout.isRepeatingAll) R.drawable.ic_repeat else R.drawable.ic_repeat_one
+    }
+
+    fun setPreviousOnClick(listener: (ImageButton) -> Unit) = this.listImageButtons[1].onClick { listener(it as ImageButton) }
+
+    fun setPlayOnClick(listener: (ImageButton) -> Unit) = this.listImageButtons[2].onClick {
+        listener(it as ImageButton)
+        this@PlayerControllerLayout.isPlayingState = !this@PlayerControllerLayout.isPlayingState
+        it.imageResource = if (this@PlayerControllerLayout.isPlayingState) R.drawable.ic_play_circle else R.drawable.ic_pause_circle
+    }
+
+    fun setNextOnClick(listener: (ImageButton) -> Unit) = this.listImageButtons[3].onClick { listener(it as ImageButton) }
+
+    fun setShuffleOnClick(listener: (ImageButton) -> Unit) = this.listImageButtons[4].onClick { listener(it as ImageButton) }
 }
