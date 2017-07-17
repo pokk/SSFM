737eb2d31fc16cdca230a4bdf31ebe1c4c7f677b
Jieyi <pokkbaby@gmail.com>
2017-07-17 20:02:47 +0900

    Added a circular seek bar.

---
+++ app/src/main/kotlin/taiwan/no1/app/ssfm/customized/CircularSeekBar.kt
@@ -0,0 +1,117 @@
+package com.example.jieyi.test.ui
+
+import android.content.Context
+import android.graphics.*
+import android.util.AttributeSet
+import android.view.MotionEvent
+import android.view.View
+import com.devrapid.kotlinknifer.logd
+import com.devrapid.kotlinknifer.logv
+import com.devrapid.kotlinknifer.logw
+import com.example.jieyi.test.R
+import org.jetbrains.anko.backgroundColor
+
+/**
+ *
+ * @author  jieyi
+ * @since   7/17/17
+ */
+class CircularSeekBar: View {
+    constructor(context: Context): super(context) {
+        init(context, null, 0)
+    }
+
+    constructor(context: Context, attrs: AttributeSet): super(context, attrs) {
+        init(context, attrs, 0)
+    }
+
+    constructor(context: Context, attrs: AttributeSet, defStyle: Int): super(context, attrs, defStyle) {
+        init(context, attrs, defStyle)
+    }
+
+    fun init(context: Context, attrs: AttributeSet?, defStyle: Int) {
+//        context.obtainStyledAttributes(attrs, R.styleable.RotatedCircleWithIconImageView, defStyle, 0).also {
+//        }.recycle()
+        backgroundColor = R.color.colorChocolate
+    }
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        setMeasuredDimension(widthMeasureSpec, heightMeasureSpec)
+    }
+
+    val p1 = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+        color = R.color.colorRed
+        strokeCap = Paint.Cap.ROUND
+        strokeWidth = 20f
+        style = Paint.Style.STROKE
+    }
+    val p2 = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+        color = R.color.colorWhite
+        strokeCap = Paint.Cap.ROUND
+        strokeWidth = 20f
+        style = Paint.Style.STROKE
+    }
+
+    val rectF by lazy { RectF(0f, 0f, width.toFloat(), height.toFloat()) }
+    val start_angle = 140f
+    val end_angle = 260f
+    val rate = end_angle / 100f
+    var progress = 0
+        set (value) {
+            field = (value * rate).toInt()
+        }
+    var temp_progress = 0
+        set(value) {
+            field = (value / rate).toInt()
+        }
+    var is_touch_btn = false
+
+    var down_x = 0f
+    var down_y = 0f
+
+    val distance: (Pair<Double, Double>, Pair<Double, Double>) -> Double = { (x1, y1), (x2, y2) ->
+        Math.sqrt(Math.pow((x1 - x2), 2.0) + Math.pow((y1 - y2), 2.0))
+    }
+
+    override fun onTouchEvent(e: MotionEvent): Boolean {
+        when (e.action) {
+            MotionEvent.ACTION_DOWN -> {
+                is_touch_btn = true
+                down_x = e.x
+                down_y = e.y
+            }
+            MotionEvent.ACTION_MOVE -> {
+                val d = distance(Pair(e.x.toDouble(), e.y.toDouble()), Pair(down_x.toDouble(), down_y.toDouble()))
+                progress = temp_progress + (d / 10f).toInt()
+                if (temp_progress + (d / 10f).toInt() >= 100)
+                    progress = 100
+                invalidate()
+            }
+            MotionEvent.ACTION_UP -> {
+                temp_progress = progress
+            }
+        }
+
+        return true
+    }
+
+    val p3 = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+        color = R.color.colorBlack
+    }
+    val pm by lazy {
+        PathMeasure().apply {
+            setPath(Path().apply { addArc(RectF(0f, 0f, width.toFloat(), height.toFloat()), start_angle, end_angle) },
+                false)
+        }
+    }
+    var pos = floatArrayOf(0f, 0f)
+
+    override fun onDraw(canvas: Canvas) {
+        canvas.drawArc(rectF, start_angle + end_angle, -end_angle + progress, false, p1)
+        canvas.drawArc(rectF, start_angle, 0f + progress, false, p2)
+
+        pm.getPosTan(pm.length / 100 * progress / rate, pos, null)
+
+        canvas.drawCircle(pos[0], pos[1], 50f, p3)
+    }
+}
