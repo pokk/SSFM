0122b2a11d647eed5d207bba60b8934db9f66b0a
Jieyi <pokkbaby@gmail.com>
2017-07-19 17:58:56 +0900

    Finished 80% in circular seek bar.

--- app/src/main/kotlin/taiwan/no1/app/ssfm/customized/CircularSeekBar.kt
+++ app/src/main/kotlin/taiwan/no1/app/ssfm/customized/CircularSeekBar.kt
@@ -5,7 +5,10 @@ import android.graphics.*
 import android.util.AttributeSet
 import android.view.MotionEvent
 import android.view.View
+import com.devrapid.kotlinknifer.logd
+import com.devrapid.kotlinknifer.logi
 import com.devrapid.kotlinknifer.logw
+import com.example.jieyi.test.R
 import org.jetbrains.anko.backgroundColor
 
 /**
@@ -14,28 +17,6 @@ import org.jetbrains.anko.backgroundColor
  * @since   7/17/17
  */
 class CircularSeekBar: View {
-    constructor(context: Context): super(context) {
-        init(context, null, 0)
-    }
-
-    constructor(context: Context, attrs: AttributeSet): super(context, attrs) {
-        init(context, attrs, 0)
-    }
-
-    constructor(context: Context, attrs: AttributeSet, defStyle: Int): super(context, attrs, defStyle) {
-        init(context, attrs, defStyle)
-    }
-
-    fun init(context: Context, attrs: AttributeSet?, defStyle: Int) {
-//        context.obtainStyledAttributes(attrs, R.styleable.RotatedCircleWithIconImageView, defStyle, 0).also {
-//        }.recycle()
-        backgroundColor = R.color.colorChocolate
-    }
-
-    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
-        setMeasuredDimension(widthMeasureSpec, heightMeasureSpec)
-    }
-
     val p1 = Paint(Paint.ANTI_ALIAS_FLAG).apply {
         color = R.color.colorRed
         strokeCap = Paint.Cap.ROUND
@@ -57,42 +38,11 @@ class CircularSeekBar: View {
         set (value) {
             field = (value * rate).toInt()
         }
-    var temp_progress = 0
-        set(value) {
-            field = (value / rate).toInt()
-        }
     var is_touch_btn = false
 
     var down_x = 0f
     var down_y = 0f
 
-    override fun onTouchEvent(e: MotionEvent): Boolean {
-        down_x = e.x
-        down_y = e.y
-
-        when (e.action) {
-            MotionEvent.ACTION_DOWN -> {
-                if (e.x in pos[0] - btn_radius..pos[0] + btn_radius && e.y in pos[1] - btn_radius..pos[1] + btn_radius)
-                    is_touch_btn = true
-                p3.color = R.color.colorCadetBlue
-            }
-            MotionEvent.ACTION_MOVE -> {
-                if (!is_touch_btn)
-                    return true
-
-                val degree = calculateTouchDegree(e.x, e.y)
-                progress = calculateTouchProgress(degree).toInt()
-                logw(progress, degree)
-
-                invalidate()
-            }
-            MotionEvent.ACTION_UP -> {
-                is_touch_btn = false
-            }
-        }
-
-        return true
-    }
 
     val p3 = Paint(Paint.ANTI_ALIAS_FLAG).apply {
         color = R.color.colorBlack
@@ -106,16 +56,25 @@ class CircularSeekBar: View {
     var pos = floatArrayOf(0f, 0f)
     var btn_radius = 50f
 
-    override fun onDraw(canvas: Canvas) {
-        canvas.drawArc(rectF, start_angle + end_angle, -end_angle + progress, false, p1)
-        canvas.drawArc(rectF, start_angle, 0f + progress, false, p2)
-
-        pm.getPosTan(pm.length / 100 * progress / rate, pos, null)
-
-        canvas.drawCircle(pos[0], pos[1], btn_radius, p3)
+    constructor(context: Context): super(context) {
+        init(context, null, 0)
     }
 
-    fun calculateTouchDegree(posX: Float, posY: Float): Double {
+    constructor(context: Context, attrs: AttributeSet): super(context, attrs) {
+        init(context, attrs, 0)
+    }
+
+    constructor(context: Context, attrs: AttributeSet, defStyle: Int): super(context, attrs, defStyle) {
+        init(context, attrs, defStyle)
+    }
+
+    fun init(context: Context, attrs: AttributeSet?, defStyle: Int) {
+//        context.obtainStyledAttributes(attrs, R.styleable.RotatedCircleWithIconImageView, defStyle, 0).also {
+//        }.recycle()
+        backgroundColor = R.color.colorChocolate
+    }
+
+    private fun calculateTouchDegree(posX: Float, posY: Float): Double {
         val x = posX - pivotX.toDouble()
         val y = posY - pivotY.toDouble()
 
@@ -125,5 +84,51 @@ class CircularSeekBar: View {
         return if (angle >= end_angle) 260.0 else angle
     }
 
-    fun calculateTouchProgress(angle: Double): Double = angle / end_angle * 100
+    private fun calculateTouchProgress(angle: Double, is_volume_up: Boolean): Double = angle / end_angle * 100
+
+    override fun onTouchEvent(e: MotionEvent): Boolean {
+        when (e.action) {
+            MotionEvent.ACTION_DOWN -> {
+                if (e.x in pos[0] - btn_radius..pos[0] + btn_radius && e.y in pos[1] - btn_radius..pos[1] + btn_radius)
+                    is_touch_btn = true
+                p3.color = R.color.colorCadetBlue
+            }
+            MotionEvent.ACTION_MOVE -> {
+                if (!is_touch_btn)
+                    return true
+
+                val down_degree = calculateTouchDegree(down_x, down_y)
+                val degree = calculateTouchDegree(e.x, e.y)
+                val is_volume_up = down_degree < degree
+
+                if (260.0 <= degree)
+                    return true
+
+                progress = calculateTouchProgress(degree, is_volume_up).toInt()
+
+                invalidate()
+            }
+            MotionEvent.ACTION_UP -> {
+                is_touch_btn = false
+            }
+        }
+
+        down_x = e.x
+        down_y = e.y
+
+        return true
+    }
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        setMeasuredDimension(widthMeasureSpec, heightMeasureSpec)
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        canvas.drawArc(rectF, start_angle + end_angle, -end_angle + progress, false, p1)
+        canvas.drawArc(rectF, start_angle, 0f + progress, false, p2)
+
+        pm.getPosTan(pm.length / 100 * progress / rate, pos, null)
+
+        canvas.drawCircle(pos[0], pos[1], btn_radius, p3)
+    }
 }
