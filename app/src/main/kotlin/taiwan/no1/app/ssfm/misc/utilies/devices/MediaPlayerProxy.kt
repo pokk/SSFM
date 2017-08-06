51776ba073baf39f4bcfe2508990933fe9de2f96
Weian <d92434101@gmail.com>
2017-08-06 18:51:16 +0800

    add getting streaming buffer percentage

--- app/src/main/kotlin/taiwan/no1/app/ssfm/misc/utilies/devices/MediaPlayerProxy.kt
+++ app/src/main/kotlin/taiwan/no1/app/ssfm/misc/utilies/devices/MediaPlayerProxy.kt
@@ -10,19 +10,23 @@ import com.devrapid.kotlinknifer.logi
  * Created by weian on 2017/6/18.
  */
 
-class MediaPlayerProxy: IMultiMediaPlayer, MediaPlayer.OnPreparedListener, MediaPlayer.OnErrorListener {
-    private var mMediaPlayer: MediaPlayer? = null
+class MediaPlayerProxy: IMultiMediaPlayer,
+        MediaPlayer.OnPreparedListener,
+        MediaPlayer.OnErrorListener,
+        MediaPlayer.OnBufferingUpdateListener {
+    private var mMediaPlayer = MediaPlayer()
     private var mState: IPlayerHandler.EPlayerState = IPlayerHandler.EPlayerState.EPlayerState_Stop
+    private var getStreamingBufferPercentage: (percentage: Int) -> Unit = {}
 
     init {
         this.mMediaPlayer = MediaPlayer()
-        this.mMediaPlayer?.setOnPreparedListener(this)
-        this.mMediaPlayer?.setOnErrorListener(this)
+        this.mMediaPlayer.setOnPreparedListener(this)
+        this.mMediaPlayer.setOnErrorListener(this)
     }
 
     override fun onPrepared(mp: MediaPlayer?) {
         logd("start playing")
-        this.mMediaPlayer?.start()
+        this.mMediaPlayer.start()
         this.mState = IPlayerHandler.EPlayerState.EPlayerState_Playing
     }
 
@@ -34,7 +38,7 @@ class MediaPlayerProxy: IMultiMediaPlayer, MediaPlayer.OnPreparedListener, Media
      * API
      */
     override fun play(uri: String) {
-        this.mMediaPlayer?.let {
+        this.mMediaPlayer.let {
             logd("prepare asynchronous thread")
             it.setDataSource(uri)
             it.prepareAsync()
@@ -43,50 +47,54 @@ class MediaPlayerProxy: IMultiMediaPlayer, MediaPlayer.OnPreparedListener, Media
 
     override fun stop() {
         logd("stop player")
-        this.mMediaPlayer?.stop()
+        this.mMediaPlayer.stop()
         this.mState = IPlayerHandler.EPlayerState.EPlayerState_Stop
     }
 
     override fun pause() {
         logd("pause player")
-        this.mMediaPlayer?.pause()
+        this.mMediaPlayer.pause()
         this.mState = IPlayerHandler.EPlayerState.EPLayerState_Pause
     }
 
     override fun resume() {
         logd("resume player")
-        this.mMediaPlayer?.start()
+        this.mMediaPlayer.start()
         this.mState = IPlayerHandler.EPlayerState.EPlayerState_Playing
     }
 
     override fun replay(is_replay: Boolean) {
         logd("set replay: " + if (is_replay) " true" else "false")
-        this.mMediaPlayer?.isLooping = is_replay
+        this.mMediaPlayer.isLooping = is_replay
     }
 
     override fun seekTo(sec: Int) {
         logd("seek time: " + sec)
-        this.mMediaPlayer?.seekTo(sec.times(1000))
+        this.mMediaPlayer.seekTo(sec.times(1000))
     }
 
     override fun duration(): Int {
         logd("get duration of media")
-        return this.mMediaPlayer?.let { it.duration.div(1000) } ?: -1
+        return this.mMediaPlayer.duration.div(1000)
     }
 
     override fun isReplay(): Boolean {
-        logd("is replay: " + if (this.mMediaPlayer?.isLooping ?: false) "true" else "false")
-        return this.mMediaPlayer?.isLooping ?: false
+        logd("is replay: " + if (this.mMediaPlayer.isLooping ?: false) "true" else "false")
+        return this.mMediaPlayer.isLooping ?: false
     }
 
     override fun isPlaying(): Boolean {
-        logd("is playing: " + if (this.mMediaPlayer?.isPlaying ?: false) " true" else " false")
-        return this.mMediaPlayer?.isPlaying ?: false
+        logd("is playing: " + if (this.mMediaPlayer.isPlaying) " true" else " false")
+        return this.mMediaPlayer.isPlaying
     }
 
     override fun current(): Int {
         logi("current time")
-        return this.mMediaPlayer?.let { it.currentPosition.div(1000) } ?: 0
+        return this.mMediaPlayer.currentPosition.div(1000)
+    }
+
+    override fun onBufferingUpdate(mp: MediaPlayer?, percent: Int) {
+        this.getStreamingBufferPercentage(percent)
     }
 
     override fun getState(): IPlayerHandler.EPlayerState {
