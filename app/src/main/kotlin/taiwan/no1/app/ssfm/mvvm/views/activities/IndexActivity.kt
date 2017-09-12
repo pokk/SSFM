2335d42425fba7f4235cac9975014622bd4e45dc
Jieyi <pokkbaby@gmail.com>
2017-09-13 08:59:36 +0900

    Added the menu index.

--- app/src/main/kotlin/taiwan/no1/app/ssfm/mvvm/views/activities/IndexActivity.kt
+++ app/src/main/kotlin/taiwan/no1/app/ssfm/mvvm/views/activities/IndexActivity.kt
@@ -3,7 +3,6 @@ package taiwan.no1.app.ssfm.mvvm.views.activities
 import android.app.Activity
 import android.os.Bundle
 import taiwan.no1.app.ssfm.R
-import taiwan.no1.app.ssfm.R.layout
 import taiwan.no1.app.ssfm.databinding.ActivityIndexBinding
 import taiwan.no1.app.ssfm.misc.widgets.MenuItem
 import taiwan.no1.app.ssfm.misc.widgets.SideMenu
@@ -21,25 +20,25 @@ class IndexActivity: AdvancedActivity<IndexViewModel, ActivityIndexBinding>() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val resideMenu = SideMenu(this).apply {
-            background = getDrawable(R.drawable.lb_background)
+        val menu = SideMenu(this).apply {
+            background = getDrawable(R.drawable.bkg_menu)
             attachActivity(this@IndexActivity)
         }
         // create menu items;
         val titles = this.resources.getStringArray(R.array.side_menu)
-        val icon = this.resources.getIntArray(R.array.ic_side_menu)
+        val icons = this.resources.getIntArray(R.array.ic_side_menu)
 
         for (i in titles.indices) {
-            val item = MenuItem(this, icon[i], titles[i])
-            resideMenu.addMenuItem(item)
+            val item = MenuItem(this, icons[i], titles[i])
+            menu.addMenuItem(item)
         }
 
-        resideMenu.openMenu()
+        menu.openMenu()
     }
 
     override fun onResume() {
         super.onResume()
     }
 
-    override fun provideBindingLayoutId(): Pair<Activity, Int> = Pair(this, layout.activity_index)
+    override fun provideBindingLayoutId(): Pair<Activity, Int> = Pair(this, R.layout.activity_index)
 }
