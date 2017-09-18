9c0bb3d6aa88ea2a9683792255791bc1a36d06d5
Jieyi <pokkbaby@gmail.com>
2017-09-18 19:44:08 +0900

    Modified the searchview event into databinding method.

--- app/src/main/kotlin/taiwan/no1/app/ssfm/mvvm/viewmodels/SearchViewModel.kt
+++ app/src/main/kotlin/taiwan/no1/app/ssfm/mvvm/viewmodels/SearchViewModel.kt
@@ -3,8 +3,6 @@ package taiwan.no1.app.ssfm.mvvm.viewmodels
 import android.app.Activity
 import android.databinding.ObservableField
 import android.view.View
-import android.widget.SearchView
-import com.devrapid.kotlinknifer.logw
 import taiwan.no1.app.ssfm.R
 
 /**
@@ -13,18 +11,21 @@ import taiwan.no1.app.ssfm.R
  * @since   9/13/17
  */
 class SearchViewModel(activity: Activity): BaseViewModel(activity) {
-    val search = ObservableField<SearchView>()
+    /** Menu Title */
     val title = ObservableField<String>()
+    /** Check search view is clicked or un-clicked */
+    val isSearching = ObservableField<Boolean>()
 
     init {
         title.set(activity.getString(R.string.menu_search))
     }
 
-    fun closeSearchView(view: View) {
-        logw("closeSearchView")
+    fun closeSearchView(): Boolean {
+        isSearching.set(false)
+        return false
     }
 
     fun openSearchView(view: View) {
-        logw("openSearchView")
+        isSearching.set(true)
     }
 }
