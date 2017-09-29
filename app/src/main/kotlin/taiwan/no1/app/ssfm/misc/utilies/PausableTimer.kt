129923dee3fd11de75df16725685c3cd16cf1724
Jieyi <pokkbaby@gmail.com>
2017-09-29 14:36:01 +0900

    Added the optimize timer.

--- app/src/main/kotlin/taiwan/no1/app/ssfm/misc/utilies/PausableTimer.kt
+++ app/src/main/kotlin/taiwan/no1/app/ssfm/misc/utilies/PausableTimer.kt
@@ -9,7 +9,6 @@ import android.os.CountDownTimer
  * @since   7/23/17
  */
 class PausableTimer(private val millisInFuture: Long = -1, private val countDownInterval: Long = 1000) {
-    
     var onTick: (millisUntilFinished: Long) -> Unit = {}
     var onFinish: () -> Unit = {}
     var isPause = false
@@ -64,6 +63,8 @@ class PausableTimer(private val millisInFuture: Long = -1, private val countDown
     private fun init(millisInFuture: Long, countDownInterval: Long) {
         timer = object: CountDownTimer(millisInFuture, countDownInterval) {
             override fun onTick(millisUntilFinished: Long) {
+                // OPTIMIZE(jieyi): 9/29/17
+                // val time = (Math.round(millisUntilFinished.toDouble() / 1000) - 1).toInt()
                 this@PausableTimer.curTime = millisUntilFinished
                 this@PausableTimer.onTick(millisUntilFinished)
             }
