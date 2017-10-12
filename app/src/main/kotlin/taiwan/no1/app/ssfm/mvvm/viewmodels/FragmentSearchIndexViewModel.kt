6058821df20a8a4f85693c08dc8a4fd4363ab2c3
Jieyi <pokkbaby@gmail.com>
2017-10-12 20:02:43 +0900

    Added the recyclerview in index search view.

--- app/src/main/kotlin/taiwan/no1/app/ssfm/mvvm/viewmodels/FragmentSearchIndexViewModel.kt
+++ app/src/main/kotlin/taiwan/no1/app/ssfm/mvvm/viewmodels/FragmentSearchIndexViewModel.kt
@@ -1,6 +1,6 @@
 package taiwan.no1.app.ssfm.mvvm.viewmodels
 
-import com.devrapid.kotlinknifer.logd
+import com.devrapid.kotlinknifer.logv
 import de.umass.lastfm.Album
 import de.umass.lastfm.Artist
 import de.umass.lastfm.Image
@@ -24,24 +24,12 @@ class FragmentSearchIndexViewModel(private val topArtistsUsecase: BaseUsecase<Co
                                    private val artistImagesUsecase: BaseUsecase<PaginatedResult<Image>, GetArtistImagesCase.RequestValue>):
     BaseViewModel() {
     fun test() {
-//        thread {
-//            lifecycleProvider.execute(topArtistsUsecase, GetTopArtistsCase.RequestValue(1)) {
-//                onNext {
-//                    it.toList()[0].mbid
-//                }
-//                onComplete {
-//
-//                }
-//            }
-//            lifecycleProvider.execute(topTracksUsecase, GetTopTracksCase.RequestValue(1)) {
-//                onNext { logw(it) }
-//            }
-//        }
-
         thread {
-            lifecycleProvider.execute(artistImagesUsecase,
-                GetArtistImagesCase.RequestValue("faf4cefb-036c-4c88-b93a-5b03dd0a0e6b")) {
-                onNext { logd(it.pageResults.toList()) }
+            lifecycleProvider.execute(topTracksUsecase) {
+                onNext { logv(it) }
+            }
+            lifecycleProvider.execute(topArtistsUsecase) {
+                onNext { logv(it) }
             }
         }
     }
