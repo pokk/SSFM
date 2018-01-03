27bca77bca7639ab17bf64ad45f3c865d909280f
Jieyi <pokkbaby@gmail.com>
2018-01-04 00:43:06 +0900

    Added the debounce library.

--- app/src/main/kotlin/taiwan/no1/app/ssfm/features/bottomsheet/BottomSheetViewModel.kt
+++ app/src/main/kotlin/taiwan/no1/app/ssfm/features/bottomsheet/BottomSheetViewModel.kt
@@ -3,59 +3,48 @@ package taiwan.no1.app.ssfm.features.bottomsheet
 import android.annotation.SuppressLint
 import android.support.design.widget.BottomSheetBehavior
 import android.view.View
-import io.reactivex.internal.operators.observable.ObservableTimer
-import io.reactivex.schedulers.Schedulers
 import taiwan.no1.app.ssfm.features.base.BaseViewModel
-import taiwan.no1.app.ssfm.misc.extension.DebounceFlag
+import taiwan.no1.app.ssfm.misc.extension.createDebounce
 import taiwan.no1.app.ssfm.misc.utilies.devices.MusicPlayerHelper
 import taiwan.no1.app.ssfm.models.usecases.AddPlaylistItemCase
-import java.util.concurrent.TimeUnit
 
 /**
  * @author  jieyi
  * @since   2017/12/17
  */
-
+@SuppressLint("CheckResult")
 class BottomSheetViewModel(private val bsHelper: BottomSheetBehavior<View>,
                            private val addPlaylistItemCase: AddPlaylistItemCase) : BaseViewModel() {
     var obtainMusicUri: String? = null
     var openDialog = {}
-    private var flag = DebounceFlag(false)
-    private var debounce = false
-
-    fun onBottomSheetDownloadClick(view: View) {
-        hideBottomSheet(view)
-        obtainMusicUri?.let {
-            MusicPlayerHelper.instance.run {
-                downloadMusic(it)
-                // TODO(jieyi): 2017/12/21 Add downloading task into the download activity.
-                addBufferPercentageListeners { }
-            }
-        } ?: throw Exception("Please Implement Correct Music Uri...")
-    }
-
-    @SuppressLint("CheckResult")
-    fun onBottomSheetAddToPlaylist(view: View) {
-        if (!debounce) {
-            debounce = true
-            ObservableTimer(300, TimeUnit.MILLISECONDS, Schedulers.newThread()).subscribe({
-                hideBottomSheet(view)
-                openDialog()
-            }, {}, { debounce = false })
+    private val debounceDownload by lazy {
+        createDebounce<View> {
+            hideBottomSheet(it)
+            obtainMusicUri?.let {
+                MusicPlayerHelper.instance.run {
+                    downloadMusic(it)
+                    // TODO(jieyi): 2017/12/21 Add downloading task into the download activity.
+                    addBufferPercentageListeners { }
+                }
+            } ?: throw Exception("Please Implement Correct Music Uri...")
         }
-//        debounce(flag) {
-//            hideBottomSheet(view)
-//            openDialog()
-//        }
     }
+    private val debounceOpenDialog by lazy {
+        createDebounce<View> {
+            hideBottomSheet(it)
+            openDialog()
+        }
+    }
+    private val debounceShare by lazy { createDebounce<View> { hideBottomSheet(it) } }
+    private val debounceCancel by lazy { createDebounce<View> { hideBottomSheet(it) } }
 
-    fun onBottomSheetShare(view: View) {
-        hideBottomSheet(view)
-    }
+    fun onBottomSheetDownloadClick(view: View) = debounceDownload.onNext(view)
 
-    fun onBottomSheetCancel(view: View) {
-        hideBottomSheet(view)
-    }
+    fun onBottomSheetAddToPlaylist(view: View) = debounceOpenDialog.onNext(view)
+
+    fun onBottomSheetShare(view: View) = debounceShare.onNext(view)
+
+    fun onBottomSheetCancel(view: View) = debounceCancel.onNext(view)
 
     private fun hideBottomSheet(view: View) {
         bsHelper.state = BottomSheetBehavior.STATE_HIDDEN
--- app/src/main/kotlin/taiwan/no1/app/ssfm/misc/extension/RxMvvmOp.kt
+++ app/src/main/kotlin/taiwan/no1/app/ssfm/misc/extension/RxMvvmOp.kt
@@ -1,22 +1,13 @@
 package taiwan.no1.app.ssfm.misc.extension
 
 import android.annotation.SuppressLint
-import io.reactivex.internal.operators.observable.ObservableTimer
-import io.reactivex.schedulers.Schedulers
+import io.reactivex.subjects.PublishSubject
 import java.util.concurrent.TimeUnit
 
 /**
  * @author  jieyi
  * @since   2018/01/03
  */
-/** Wrapper for passing by reference and letting us modify the flag variable. */
-data class DebounceFlag(var flag: Boolean)
-
 @SuppressLint("CheckResult")
-inline fun debounce(debounce: DebounceFlag, crossinline block: () -> Unit) {
-    if (!debounce.flag) {
-        debounce.flag = true
-        ObservableTimer(300, TimeUnit.MILLISECONDS, Schedulers.newThread()).
-            subscribe({ block() }, {}, { debounce.flag = false })
-    }
-}
+fun <T> createDebounce(block: (param: T) -> Unit) =
+    PublishSubject.create<T>().apply { debounce(300, TimeUnit.MILLISECONDS).subscribe(block) }
