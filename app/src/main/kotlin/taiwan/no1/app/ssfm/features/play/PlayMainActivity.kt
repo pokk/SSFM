60ccaf0d11a839943b9466b494cf2affddc71776
Jieyi <pokkbaby@gmail.com>
2018-02-04 12:03:31 +0900

    Added the control the play and stop button.

--- app/src/main/kotlin/taiwan/no1/app/ssfm/features/play/PlayMainActivity.kt
+++ app/src/main/kotlin/taiwan/no1/app/ssfm/features/play/PlayMainActivity.kt
@@ -1,15 +1,13 @@
 package taiwan.no1.app.ssfm.features.play
 
-import android.app.Activity
 import android.os.Bundle
+import com.devrapid.kotlinknifer.logw
 import kotlinx.android.synthetic.main.part_main_play_music.rcii_album
 import taiwan.no1.app.ssfm.R
 import taiwan.no1.app.ssfm.databinding.ActivityMusicBinding
 import taiwan.no1.app.ssfm.features.base.AdvancedActivity
 import taiwan.no1.app.ssfm.misc.utilies.devices.helper.music.playerHelper
-import taiwan.no1.app.ssfm.models.entities.lastfm.AlbumEntity
-import taiwan.no1.app.ssfm.models.entities.lastfm.ArtistEntity
-import taiwan.no1.app.ssfm.models.entities.lastfm.BaseEntity
+import weian.cheng.mediaplayerwithexoplayer.MusicPlayerState
 import javax.inject.Inject
 
 /**
@@ -19,35 +17,24 @@ import javax.inject.Inject
  */
 class PlayMainActivity : AdvancedActivity<PlayMainViewModel, ActivityMusicBinding>() {
     @Inject override lateinit var viewModel: PlayMainViewModel
-    private val path: Array<String> = arrayOf("https://soundsthatmatterblog.files.wordpress.com/2012/12/04-just-give-me-a-reason-feat-nate-ruess.mp3",
-                                              "/storage/emulated/0/Download/test.mp3")
-    private var albumRes = mutableListOf<BaseEntity>(
-        AlbumEntity.AlbumWithArtist(ArtistEntity.Artist(), "0"),
-        AlbumEntity.AlbumWithArtist(ArtistEntity.Artist(), "0"),
-        AlbumEntity.AlbumWithArtist(ArtistEntity.Artist(), "0"),
-        AlbumEntity.AlbumWithArtist(ArtistEntity.Artist(), "0"),
-        AlbumEntity.AlbumWithArtist(ArtistEntity.Artist(), "0"),
-        AlbumEntity.AlbumWithArtist(ArtistEntity.Artist(), "0"),
-        AlbumEntity.AlbumWithArtist(ArtistEntity.Artist(), "0"),
-        AlbumEntity.AlbumWithArtist(ArtistEntity.Artist(), "0"),
-        AlbumEntity.AlbumWithArtist(ArtistEntity.Artist(), "0"),
-        AlbumEntity.AlbumWithArtist(ArtistEntity.Artist(), "0"),
-        AlbumEntity.AlbumWithArtist(ArtistEntity.Artist(), "0"),
-        AlbumEntity.AlbumWithArtist(ArtistEntity.Artist(), "0"),
-        AlbumEntity.AlbumWithArtist(ArtistEntity.Artist(), "0"))
 
     //region Activity lifecycle
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         rcii_album.apply {
+            logw(playerHelper.state, playerHelper.currentTime, playerHelper.trackDuration)
+            if (MusicPlayerState.Play == playerHelper.state) start()
             startTime = playerHelper.currentTime
             endTime = playerHelper.trackDuration
-
+            onClickEvent = { view, isPause ->
+                // OPTIMIZE(jieyi): 2018/02/04 The library has some issues which cant upload new version now.
+                playerHelper.apply { if (isPause) pause() else play() }
+            }
         }
     }
     //endregion
 
     //region Base activity implement
-    override fun provideBindingLayoutId(): Pair<Activity, Int> = Pair(this, R.layout.activity_music)
+    override fun provideBindingLayoutId() = this to R.layout.activity_music
     //endregion
 }
