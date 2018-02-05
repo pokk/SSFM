b1ccdea79686a442413826fe01634c76f942c0e4
Jieyi <pokkbaby@gmail.com>
2018-02-05 21:00:22 +0900

    Removed the redundant code.

--- app/src/main/kotlin/taiwan/no1/app/ssfm/features/play/PlayMainActivity.kt
+++ app/src/main/kotlin/taiwan/no1/app/ssfm/features/play/PlayMainActivity.kt
@@ -1,13 +1,11 @@
 package taiwan.no1.app.ssfm.features.play
 
 import android.os.Bundle
-import com.devrapid.kotlinknifer.logw
 import kotlinx.android.synthetic.main.part_main_play_music.rcii_album
 import taiwan.no1.app.ssfm.R
 import taiwan.no1.app.ssfm.databinding.ActivityMusicBinding
 import taiwan.no1.app.ssfm.features.base.AdvancedActivity
 import taiwan.no1.app.ssfm.misc.utilies.devices.helper.music.playerHelper
-import weian.cheng.mediaplayerwithexoplayer.MusicPlayerState
 import javax.inject.Inject
 
 /**
@@ -22,12 +20,9 @@ class PlayMainActivity : AdvancedActivity<PlayMainViewModel, ActivityMusicBindin
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         rcii_album.apply {
-            logw(playerHelper.state, playerHelper.currentTime, playerHelper.trackDuration)
-            if (MusicPlayerState.Play == playerHelper.state) start()
             startTime = playerHelper.currentTime
             endTime = playerHelper.trackDuration
             onClickEvent = { view, isPause ->
-                // OPTIMIZE(jieyi): 2018/02/04 The library has some issues which cant upload new version now.
                 playerHelper.apply { if (isPause) pause() else play() }
             }
         }
