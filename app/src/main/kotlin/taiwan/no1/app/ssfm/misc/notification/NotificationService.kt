ffc4f5d0652b7d05e0b975e9fa6a9de277e9aa47
Jieyi <pokkbaby@gmail.com>
2018-03-21 01:45:59 +0900

    Modified the notification.

--- app/src/main/kotlin/taiwan/no1/app/ssfm/misc/notification/NotificationService.kt
+++ app/src/main/kotlin/taiwan/no1/app/ssfm/misc/notification/NotificationService.kt
@@ -1,16 +1,12 @@
 package taiwan.no1.app.ssfm.misc.notification
 
-import android.app.Notification
 import android.app.NotificationManager
 import android.app.PendingIntent
 import android.app.Service
 import android.content.Context
 import android.content.Intent
 import android.os.Binder
-import android.os.IBinder
 import android.support.v4.app.NotificationCompat
-import android.util.Log
-import com.hwangjr.rxbus.RxBus
 import taiwan.no1.app.ssfm.R
 import taiwan.no1.app.ssfm.misc.utilies.devices.helper.music.MusicPlayerHelper
 
@@ -21,10 +17,9 @@ import taiwan.no1.app.ssfm.misc.utilies.devices.helper.music.MusicPlayerHelper
  */
 
 class NotificationService : Service() {
-
     private lateinit var notificationManager: NotificationManager
-    private lateinit var notificationBuilder: Notification.Builder
-    private lateinit var notification: Notification
+    private lateinit var notificationBuilder: NotificationCompat.Builder
+    private lateinit var notification: NotificationCompat
     private val binder = NotificationBinder()
     private var musicPlayerHelper: MusicPlayerHelper? = null
 
@@ -33,9 +28,24 @@ class NotificationService : Service() {
     private val PLAYER_ICON_PAUSE = R.drawable.ic_pause
     private val PLAYER_ICON_FORWARD = R.drawable.ic_skip_next
 
-    private lateinit var actionPlay: Notification.Action
-    private lateinit var actionPause: Notification.Action
-    private lateinit var actionNext: Notification.Action
+    private val actionPlay by lazy {
+        val intentPlay = Intent(this, NotificationService::class.java).apply { action = ACTION_PLAY }
+        val pendingIntentPlay = PendingIntent.getService(this, 0, intentPlay, 0)
+
+        NotificationCompat.Action.Builder(PLAYER_ICON_PLAY, "Play", pendingIntentPlay).build()
+    }
+    private val actionPause by lazy {
+        val intentPause = Intent(this, NotificationService::class.java).apply { action = ACTION_PAUSE }
+        val pendingIntentPause = PendingIntent.getService(this, 0, intentPause, 0)
+
+        NotificationCompat.Action(PLAYER_ICON_PAUSE, "Pause", pendingIntentPause)
+    }
+    private val actionNext by lazy {
+        val intentForward = Intent(this, NotificationService::class.java).apply { action = ACTION_NEXT }
+        val pendingIntentForward = PendingIntent.getService(this, 0, intentForward, 0)
+
+        NotificationCompat.Action(PLAYER_ICON_FORWARD, "Next", pendingIntentForward)
+    }
 
     companion object {
         private const val NOTIFY_ID = 0
@@ -44,44 +54,21 @@ class NotificationService : Service() {
         private const val ACTION_PAUSE = "ACTION_PAUSE"
     }
 
-    init {
-        RxBus.get().register(this)
-    }
-
     fun initNotification() {
-
         notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
-        // action play
-        val intentPlay = Intent(this, NotificationService::class.java)
-        intentPlay.action = ACTION_PLAY
-        val pendingIntentPlay = PendingIntent.getService(this, 0, intentPlay, 0)
-        actionPlay = Notification.Action.Builder(PLAYER_ICON_PLAY, "Play", pendingIntentPlay).build()
-
-        // action pause
-        val intentPause = Intent(this, NotificationService::class.java)
-        intentPause.action = ACTION_PAUSE
-        val pendingIntentPause = PendingIntent.getService(this, 0, intentPause, 0)
-        actionPause = Notification.Action(PLAYER_ICON_PAUSE, "Pause", pendingIntentPause)
-
-        // action forward
-        val intentForward = Intent(this, NotificationService::class.java)
-        intentForward.action = ACTION_NEXT
-        val pendingIntentForward = PendingIntent.getService(this, 0, intentForward, 0)
-        actionNext = Notification.Action(PLAYER_ICON_FORWARD, "Next", pendingIntentForward)
-
-        notificationBuilder = Notification.Builder(applicationContext)
-                .setSmallIcon(PLAYER_ICON)
-                .setContentTitle(applicationContext.packageName)
-                .addAction(actionPlay)
-                .addAction(actionNext)
+        notificationBuilder = NotificationCompat.Builder(applicationContext, "test")
+            .setSmallIcon(PLAYER_ICON)
+            .setContentTitle(applicationContext.packageName.split(".").last())
+            .addAction(actionPlay)
+            .addAction(actionNext)
+            .setOngoing(true)
 
         notificationManager.notify(NOTIFY_ID, notificationBuilder.build())
     }
 
     fun removeNotification() {
         notificationManager.cancel(NOTIFY_ID)
-        RxBus.get().unregister(this)
     }
 
     fun setMusicPlayerHelper(helper: MusicPlayerHelper) {
@@ -100,7 +87,7 @@ class NotificationService : Service() {
         musicPlayerHelper?.pause()
     }
 
-    override fun onBind(intent: Intent?): IBinder = binder
+    override fun onBind(intent: Intent?) = binder
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
 
